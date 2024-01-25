6ff1b308c18ff7cd4c54d61bf936dcc366cbc2a3
Danna Bohnhoff <dannab101@gmail.com>
2024-01-25 06:18:59 -0500

    database

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-# app.py
 import bcrypt
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_username:your_password@localhost/your_database'
+CORS(app)  # Enable CORS
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/your_database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
